2bc4066fc44251d20b105eeae180fa9e85d06dbf
Juan <santiljuan179@gmail.com>
2022-11-17 16:49:09 -0400

    beta

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Inicio from "./pages/Inicio";
 import "./App.css";
 
 function App() {
+  const ad = 67;
   return (
     <div className="App">
       <Inicio />
--- src/Components/Section.jsx
+++ src/Components/Section.jsx
@@ -15,36 +15,56 @@ function Section() {
   const previusCard = () => {
     setCard(card === 0 ? cantidad - 1 : card - 1);
   };
+
   return (
     <div className="Container">
       <h2 className="Title">
         VIAJA CON NOSOTROS Y DISFRUTA DE LOS MEJORES PAISAJES <br /> ¡AL MEJOR
         PRECIO!
       </h2>
-      <div className="Container-btn">
+
+      <div className="Container-card ">
         <button onClick={previusCard} className="Btn">
           <FaChevronCircleLeft />
         </button>
-        <div className="Container-card">
-          {Data.map((Data, index) => {
-            return (
-              <div
-                key={index}
-                className={card === index ? "slider active" : "slider"}
-              >
-                {card === index && (
+        {screen.width <= 750 ? (
+          <div className="Container-card">
+            {Data.map((Data, index) => {
+              return (
+                <div
+                  key={index}
+                  className={card === index ? "slider active" : "slider"}
+                >
+                  {card === index && (
+                    <Card
+                      className="Card"
+                      key={index}
+                      Img={Data.Img}
+                      Pais={Data.Pais}
+                      Description={Data.Description}
+                    />
+                  )}
+                </div>
+              );
+            })}
+          </div>
+        ) : (
+          <div className="Container-card">
+            {Data.map((Data) => {
+              return (
+                <div>
                   <Card
                     className="Card"
-                    key={index}
                     Img={Data.Img}
                     Pais={Data.Pais}
                     Description={Data.Description}
                   />
-                )}
-              </div>
-            );
-          })}
-        </div>
+                </div>
+              );
+            })}
+          </div>
+        )}
+
         <button onClick={nextCard} className="Btn">
           <FaChevronCircleRight />
         </button>
